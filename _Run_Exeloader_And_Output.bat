53e9731f23a9ce9454ebe6dc001b7527a8fa788a
Maeiky <maeiky@gmail.com>
2020-12-08 12:53:55 -0500

    Add auto exit

--- _Run_Exeloader_And_Output.bat
+++ _Run_Exeloader_And_Output.bat
@@ -1,4 +1,4 @@
 echo off
 REM https://www.dosbox.com/wiki/Usage
-start DOSBox.exe  -c "mount c '%~dp0'"	-c C:	-c "Exeloa~1.exe shapes~1.exe > _output.txt" > log.txt -noconsole
+start DOSBox.exe  -c "mount c '%~dp0'"	-c C:	-c "Exeloa~1.exe shapes~1.exe > _output.txt"  -c exit > log.txt -noconsole
 
